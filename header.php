e3d285c9c8542899c690a6ca60023c2c0ea25dd6
imad <imadidaliouali@gmail.com>
2022-11-26 20:26:43 +0100

    remove the underline in a tag

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
 <header>
     <h1>Hanouti</h1>
-    <a href="/webgi2/Hanouti/index.php?logout=1">Log out</a>
+    <button id="logout"><a href="/webgi2/Hanouti/index.php?logout=1">Log out</a></button>
 </header>
